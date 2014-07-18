ab6b86fc5e0a43dc151b59ff9f1685258a872329
Alejandro Llaves <allaves@fi.upm.es>
2014-07-18 14:47:08 +0200

    Tests with FeederSpout and FixedBatchSpout using Trident.

--- test/topology/TridentJoinExample.java
+++ test/topology/TridentJoinExample.java
@@ -1,11 +1,14 @@
 package topology;
 
+import java.sql.Date;
+
 import backtype.storm.Config;
 import backtype.storm.LocalCluster;
 import backtype.storm.StormSubmitter;
 import backtype.storm.generated.AlreadyAliveException;
 import backtype.storm.generated.InvalidTopologyException;
 import backtype.storm.generated.StormTopology;
+import backtype.storm.testing.FeederSpout;
 import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Values;
 import backtype.storm.utils.Utils;
@@ -22,31 +25,37 @@ public class TridentJoinExample {
 	public static void main(String[] args) {
 		
 		// This spouts emits the same observations over and over again
-		FixedBatchSpout observationSpout = new FixedBatchSpout(new Fields("obsId", "observedProperty", "value", "uom", "timestamp", "sensorId"), 3,
-				new Values(1, "temperature", 35.0, "degrees Celsius", System.currentTimeMillis(), "sensor1"), 
-				new Values(2, "temperature", 33.3, "degrees Celsius", System.currentTimeMillis(), "sensor5"), 
-				new Values(3, "temperature", 34.1, "degrees Celsius", System.currentTimeMillis(), "sensor2"),
-				new Values(4, "temperature", 35.2, "degrees Celsius", System.currentTimeMillis(), "sensor1"),
-				new Values(5, "temperature", 31.4, "degrees Celsius", System.currentTimeMillis(), "sensor2"));
-		observationSpout.setCycle(true);
+//		FixedBatchSpout observationSpout = new FixedBatchSpout(new Fields("obsId", "observedProperty", "value", "uom", "timestamp", "sensorId"), 5,
+//				new Values("obs1", "temperature", 35.0, "degrees Celsius", System.currentTimeMillis(), 1), 
+//				new Values("obs2", "temperature", 33.3, "degrees Celsius", System.currentTimeMillis(), 2), 
+//				new Values("obs3", "temperature", 34.1, "degrees Celsius", System.currentTimeMillis(), 3),
+//				new Values("obs4", "temperature", 35.2, "degrees Celsius", System.currentTimeMillis(), 4),
+//				new Values("obs5", "temperature", 31.4, "degrees Celsius", System.currentTimeMillis(), 5));
+//		observationSpout.setCycle(true);
 		
 		// This spouts emits the same sensor locations over and over again
-		FixedBatchSpout sensorSpout = new FixedBatchSpout(new Fields("sensorId", "lat", "lon"), 3, 
-				new Values("sensor1", "40.4055381", "-3.8399521"), 
-				new Values("sensor2", "40.4055382", "-3.8399522"),
-				new Values("sensor3", "40.4055383", "-3.8399523"),
-				new Values("sensor4", "40.4055384", "-3.8399524"),
-				new Values("sensor5", "40.4055385", "-3.8399525"),
-				new Values("sensor6", "40.4055386", "-3.8399526"),
-				new Values("sensor7", "40.4055387", "-3.8399527"));
-		sensorSpout.setCycle(true);
+//		FixedBatchSpout sensorSpout = new FixedBatchSpout(new Fields("sensorId", "lat", "lon"), 5, 
+//				new Values(1, "40.4055381", "-3.8399521"), 
+//				new Values(2, "40.4055382", "-3.8399522"),
+//				new Values(3, "40.4055383", "-3.8399523"),
+//				new Values(4, "40.4055384", "-3.8399524"),
+//				new Values(5, "40.4055385", "-3.8399525"),
+//				new Values(6, "40.4055386", "-3.8399526"),
+//				new Values(7, "40.4055387", "-3.8399527"));
+//		sensorSpout.setCycle(true);
+		
+		
+		FeederSpout observationSpout = new FeederSpout(new Fields("obsId", "observedProperty", "value", "uom", "timestamp", "sensorId"));
+	    FeederSpout sensorSpout = new FeederSpout(new Fields("sensorId", "lat", "lon"));
+		
+		
 		
 		// Topology and stream definition
 		TridentTopology tridentTopology = new TridentTopology();
 		Stream observationStream = tridentTopology.newStream("observationStream", observationSpout);
-		Stream sensorStream = tridentTopology.newStream("sensorStream", observationSpout);
+		Stream sensorStream = tridentTopology.newStream("sensorStream", sensorSpout);
 		tridentTopology.join(observationStream, new Fields("sensorId"), sensorStream, new Fields("sensorId"), 
-				new Fields("obsId", "observedProperty", "value", "uom", "timestamp", "lat", "lon"));
+				new Fields("sensorId", "obsId", "observedProperty", "value", "uom", "timestamp", "lat", "lon"));
 		
 		
 		Config conf = new Config();
@@ -57,7 +66,17 @@ public class TridentJoinExample {
 		// Cannot see the joins
 	    cluster.submitTopology("tridentJoinExample", conf, tridentTopology.build());
 	    
-	    Utils.sleep(5000);
+	    // Simulate data - Feed the spouts
+	    for (int i = 9; i >= 0; i--) {
+	    	sensorSpout.feed(new Values(i, "40.4055385", "-3.8399527"));
+	    }
+	    
+	    for (int i = 0; i < 50; i++) {
+	    	observationSpout.feed(new Values(i, "temperature", Math.random()%10, "degrees Celsius", new Date(System.currentTimeMillis()), (i+1)/10));
+	    }
+	    
+	    
+	    Utils.sleep(10000);
 	    cluster.shutdown();
 	}
 	
